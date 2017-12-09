922e09618202140d378adda022df88c726b40bf4
YalovikEA <yalovikea@gmail.com>
2017-12-09 14:57:06 +0300

    add test project

---
+++ dev/static/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    svg4everybody({});
+});
+
+$(document).ready(function () {
+  // Слайдер на главной странице
+  $('.js-index-slider').slick({
+    prevArrow: '.index-slider__controls--prev',
+    nextArrow: '.index-slider__controls--next',
+    dots: true,
+    customPaging : function() {
+      return '<a class="index-slider__dots"></a>';
+    },
+  });
+
+  // Слайдер рекомендованных товаров
+  $('.js-recommended-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    appendDots: '.recommended-slider__nav',
+    prevArrow: '.recommended-slider__controls--prev',
+    nextArrow: '.recommended-slider__controls--next',
+    customPaging : function() {
+      return '<a class="recommended-slider__dot"></a>';
+    },
+  });
+
+  // Позиционируем управление слайдера на главной странице
+  function sliderElemPos(elem,pos) {
+    var windowWidth = $(window).width(),
+        containerWidth = $('.container').width(),
+        position = (windowWidth-containerWidth)/2;
+    $('.index-slider ' + elem).css(pos,position);
+  }
+
+  // Позиционируем точки слайдера на главной странице
+  sliderElemPos('.slick-dots','left');
+
+  // Позиционируем стрелки слайдера на главной странице
+  sliderElemPos('.index-slider__controls','right');
+
+  // Функции при ресайзе
+  $(window).resize(function () {
+    sliderElemPos('.slick-dots','left');
+    sliderElemPos('.index-slider__controls','right');
+  })
+});
+
+$('.video-start').click(function () {
+  var videoID = $(this).parent().parent().data('video-id'),
+      videoPlayer = $(this).parent().parent().attr('id');
+  player = new YT.Player(videoPlayer, {
+    videoId: videoID
+  });
+});
